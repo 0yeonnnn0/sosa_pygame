b4839618123d9e0a70eb3071e75f9112314a9fbe
lemonsoda1 <angrybird2600@gmail.com>
2023-05-31 12:36:24 +0900

    2nd file

--- main.py
+++ main.py
@@ -10,15 +10,30 @@ def collision(obj1, obj2):
         return True
     return False
 
+def draw_text(txt, size, pos, color):
+    font = pygame.font.Font("freesansbold.ttf", size)
+    r = font.render(txt, True, color)
+    screen.blit(r, pos)
+
+    txt = "Time : {:.1f}, Bullets:{}".format(time.time() - start_time, len(bullets))
+    draw_text(txt, 32, (10, 10), (255, 255, 255))
+
+bg_image = pygame.image.load("pygame_dodge/dodge_src/bg.jpg")
+bg_pos = 0
+
+#background music, -1 : 무한반복
+#pygame.mixer.music.load("pygame_dodge/dodge_src/bgm.wav")
+#pygame.mixer.music.play(-1)
+
 pygame.init()
-WIDTH, HEIGHT = 1000, 800
+WIDTH, HEIGHT = 500, 400
 
 pygame.display.set_caption("총알 피하기")
 
 clock = pygame.time.Clock()
 FPS = 60
 
-player = Player(WIDTH/2, HEIGHT/2)
+time_for_adding_bullets = 0
 
 #총알 생성
 bullets = []
@@ -26,13 +41,16 @@ for i in range(10):
     bullets.append(Bullet(0, rnd.random()*HEIGHT, rnd.random()-0.5, rnd.random()-0.5))
 
 #그림 불러오기
-image = pygame.image.load("총알피하기/리소스/player.png")
+image = pygame.image.load("pygame_dodge/dodge_src/player.png")
 #그림 크기조절
 image = pygame.transform.scale(image, (128, 128))
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+player = Player(screen)
 
+gameover = False
+score = 0
 #game Loop
 running = True
 while running:
@@ -43,39 +61,50 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print("왼쪽 방향키 누름")
                 player.goto(-1, 0)
             elif event.key == pygame.K_RIGHT:
-                print("오른쪽 방향키 누름")
                 player.goto(1, 0)
             elif event.key == pygame.K_UP:
-                print("윗쪽 방향키 누름")
+                player.goto(0, -1)
             elif event.key == pygame.K_DOWN:
-                print("아래쪽 방향키 누름")
+                player.goto(0, 1)
  
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
-                print("왼쪽 방향키 뗌")
                 player.goto(1, 0) 
             elif event.key == pygame.K_RIGHT:
-                print("오른쪽 방향키 뗌")
                 player.goto(-1, 0)
             elif event.key == pygame.K_UP:
-                print("윗쪽 방향키 뗌")
+                player.goto(0, 1)
             elif event.key == pygame.K_DOWN:
-                print("아래쪽 방향키 뗌")
-    for b in bullets:
-        b.update_and_draw(dt, screen)
+                player.goto(0, -1)
 
-    #화면에 검은색 채우기
-    screen.fill((0,0,0))
+    time_for_adding_bullets += dt
+    if time_for_adding_bullets > 1000:
+        bullets.append(Bullet(0, rnd.random()*HEIGHT, rnd.random()-0.5, rnd.random()-0.5,))
+        time_for_adding_bullets -= 1000
+
+    bg_pos -= 0.01 * dt
+    screen.blit(bg_image, (bg_pos, 0))
 
     player.update(dt, screen)
     player.draw(screen)
 
+    if gameover:    
+        draw_text("GAME OVER", 100, (WIDTH/2 - 300, HEIGHT/2 -50, (255, 255, 255)))
+        txt = "Time: {:.1f} Bullets:{}".format(score, len(bullets))
+        draw_text(txt, 32, (WIDTH/2 -150, HEIGHT/2 +50), (255, 255, 255))
+    else:
+        score = time.time() - start_time
+        txt = "Time: {:.1f} Bullets:{}".format(score, len(bullets))
+        draw_text(txt, 32, (10, 10), (255, 255, 255))
+
     pygame.display.update()         #이걸 작성해야 화면이 바뀜!
 
     for b in bullets:
+        b.update_and_draw(dt, screen)
         if collision(player, b):
             time.sleep(2)
             running = False
+
+    
--- player.py
+++ player.py
@@ -1,26 +1,35 @@
 import pygame
 
 class Player:
-    def __init__(self, x, y):
-        self.image = pygame.image.load("총알피하기/리소스/player.png")
-        self.image = pygame.transform.scale(self.image, (64, 64))
-        self.pos = [x, y]
+    def __init__(self, screen):
+        width, height = screen.get_size()
+        self.image = pygame.image.load("pygame_dodge/dodge_src/player.png")
+        self.image = pygame.transform.scale(self.image, (32, 32))
+        self.pos = [width/2, height/2]
         self.to = [0, 0]
-        self.acc = [0, 0]
+        self.angle = 0
 
     def draw(self, screen):
+        calib_pos = (
+            self.pos[0] - self.image.get_size()[0]/2,
+            self.pos[1] - self.image.get_size()[1]/2
+        )
+
         if self.to == [-1, -1]: self.angle = 45
         elif self.to == [-1, 0]: self.angle = 90
         elif self.to == [-1, 1]: self.angle = 135
         elif self.to == [0, 1]: self.angle = 180
-        elif self.to == [1, 1]: self.angle = -135
-        elif self.to == [1, 0]: self.angle = -90
-        elif self.to == [1, -1]: self.angle = -45
+        elif self.to == [1, 1]: self.angle = 225
+        elif self.to == [1, 0]: self.angle = 270
+        elif self.to == [1, -1]: self.angle = 315
         elif self.to == [0, -1]: self.angle = 0
 
         rotated = pygame.transform.rotate(self.image, self.angle)
-        calib_pos = (self.pos[0] - rotated.get_width()/2, self.pos[1] - rotated.get_height()/2)
-        screen.blit(rotated_image, calib_pos)
+        calib_pos = (
+            self.pos[0] - rotated.get_size()[0]/2, 
+            self.pos[1] - rotated.get_size()[1]/2
+            )
+        screen.blit(rotated, calib_pos)
     
     def goto(self, x, y):
         self.to[0] += x
@@ -28,7 +37,18 @@ class Player:
     
     def update(self, dt, screen):
         width, height = screen.get_size()
-        self.pos[0] = self.pos[0] + dt*self.to[0]    #x좌표
-        self.pos[1] = self.pos[1] + dt*self.to[1]    #y좌표
-        self.pos[0] = min(max(self.pos[0], 32), width-32)
-        self.pos[1] = min(max(self.pos[1], 32), height-32)
+        self.pos[0] = (self.pos[0] + dt * self.to[0] * 0.5)
+        self.pos[1] = (self.pos[1] + dt * self.to[1] * 0.5)
+
+        #화면 밖으로 나가지 않게, 16은 기체의 절반크기
+        if self.pos[0] < 0:
+            self.pos[0] = 16
+        if self.pos[0] > width:
+            self.pos[0] = width-1
+        if self.pos[1] < 0:
+            self.pos[1] = 16
+        if self.pos[1] > height:
+            self.pos[1] = height-16
+        
+        #self.pos[0] = min(width-16, max(16, self.pos[0]))
+        #self.pos[1] = min(height-16, max(16, self.pos[1]))
